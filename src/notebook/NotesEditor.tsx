c134217080f366500348634554f6b7f917df0fb7
keshavagr025 <keshavagr204@gmail.com>
2025-12-18 23:12:39 +0530

    Checking work flow

--- src/notebook/NotesEditor.tsx
+++ src/notebook/NotesEditor.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotesEditor = () => {
   return (
-    <textarea className='w-full h-full p-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400' placeholder='Write your notes here...'></textarea>
+    <div>Notes Editor Component</div>
   )
 }
 
